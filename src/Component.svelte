8443d51e4b2ed1fc26a27e13151c0ae142a2bef8
6rak0 <ego.6rak0@gmail.com>
2021-01-10 16:32:33 -0600

    first commit

---
+++ src/Component.svelte
@@ -0,0 +1,25 @@
+<script>
+  import loginUser from "./lib/helper";
+  let identifier = "";
+  let password = "";
+  async function handleSubmit() {
+    let user;
+    user = await loginUser({ identifier, password });
+  }
+</script>
+
+<section>
+  <form on:submit|preventDefault={handleSubmit}>
+    <div>
+      <input type="text" placeholder="email" required bind:value={identifier} />
+    </div>
+    <div>
+      <input
+        type="password"
+        placeholder="password"
+        required
+        bind:value={password} />
+    </div>
+    <button type="submit">login</button>
+  </form>
+</section>
